574fcec3ff5d0c2d5dd8ffa001693a4f662bf10c
LucasChizzoli <chizzoli.lucas13@gmail.com>
2022-04-14 20:39:50 -0300

    Add user relation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  validates :title, :content, presence: true
+
+  belongs_to :user
+  
   def self.search(query)
     where("title ILIKE ?", "%#{sanitize_sql_like(query)}%")
   end
