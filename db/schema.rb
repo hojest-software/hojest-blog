aa5a332805dbebc5d63c970634c623f0722c0f69
LucasChizzoli <chizzoli.lucas13@gmail.com>
2022-04-15 19:38:36 -0300

    Configure admin and cancancan

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_15_003235) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_15_181651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_003235) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "topics", default: [], array: true
+    t.integer "likes", default: 0
     t.index ["user_id"], name: "index_user_id"
   end
 
@@ -35,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_003235) do
     t.text "about"
     t.string "social_link"
     t.string "name"
+    t.boolean "admin_role", default: false
+    t.boolean "user_role", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
