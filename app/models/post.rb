842e6dc16ebd04e953cc6f0f732b6f42661973c9
LucasChizzoli <chizzoli.lucas13@gmail.com>
2022-04-17 15:30:35 -0300

    Add post description

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,11 @@
 
 class Post < ApplicationRecord
   validates :title, :content, presence: true
+  validates :description, presence: true, length: { maximum: 130 }
 
   belongs_to :user
+
+  has_rich_text :content
   
   def self.search(query)
     where("title ILIKE ?", "%#{sanitize_sql_like(query)}%").where(published: true)
