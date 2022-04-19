ff53c33eea3cb0e481de46132e0a892493bf13f9
LucasChizzoli <chizzoli.lucas13@gmail.com>
2022-04-19 13:31:08 -0300

    Rever post order by desc

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,8 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = search_params.empty? ?
-      Post.all.order(:created_at).select(&:published) :
-      Post.search(search_params[:query]).order(:created_at)
+      Post.all.order(created_at: :desc).select(&:published) :
+      Post.search(search_params[:query]).order(created_at: :desc)
   end
 
   # GET /posts/1 or /posts/1.json
@@ -77,6 +77,6 @@ class PostsController < ApplicationController
     end
 
     def parse_topics
-      params[:topics].split(',')
+      post_params[:topics].split(',').map { |topic| topic.strip }
     end
 end
