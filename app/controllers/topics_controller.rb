66c8b7c346937649e3a97994ad287bd18472134a
LucasChizzoli <chizzoli.lucas13@gmail.com>
2022-04-19 13:31:23 -0300

    Strip topcis to remove duplications

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,6 +2,6 @@ class TopicsController < ApplicationController
   skip_before_action :authenticate_user!
   
   def index
-    @topics ||= Post.where(published: true).map(&:topics).flatten.uniq
+    @topics ||= Post.where(published: true).map(&:topics).flatten.map { |s| s.strip }.uniq
   end
 end
